db4bc05d5840a9a6d8543ad5d539488fda4c6e79
Mateo <mateo2aponte@hotmail.com>
2024-08-25 00:03:59 -0500

    fix: Add log

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+console.log(fileURLToPath(new URL('./src', import.meta.url)));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
